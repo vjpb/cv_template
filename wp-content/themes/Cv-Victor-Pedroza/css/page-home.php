535aad238beba054eb1e7bb75ffda32f0a26be03
Victor Pedroza <vjpb881104@gmail.com>
2020-05-11 23:01:17 -0500

    modified header,layout & in the page-home a new header was made

--- wp-content/themes/Cv-Victor-Pedroza/css/page-home.php
+++ wp-content/themes/Cv-Victor-Pedroza/css/page-home.php
@@ -1,5 +1,28 @@
-<?php /* Template Name: Home */ ?>
+    <?php /* Template Name: Home */ ?>
 
-<?php get_header(); ?>
+    <?php get_header(); ?>
 
-<?php get_footer(); ?>
+        <div class ="my-photo"> 
+          
+            <div class="row banner">
+
+                <div class="banner-text">
+
+                    <h1 class="responsive-headline">¡Hola! soy Victor Pedroza.</h1>
+                    <h3>Soy profesional en <span>Ingenieria de petroleo</span>, <span>desarrolador web</span> y <span>un emprendedor</span> curioso por nuevas tecnologias, estudiando y aprendiendo por mi cuenta para realizar mis metas. <a href="<?php echo get_site_url(); ?>/about">Empieza a desplazarte </a>y lee un poco mas <a href="<?php echo get_site_url(); ?>/about">acerca de mi</a>.</h3>
+                    <hr />
+                    <ul class="social">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                    </ul>
+                </div>
+            </div>  
+        </div>      
+</header> <!-- Header End -->
+    
+    <?php get_footer(); ?>
--- wp-content/themes/Cv-Victor-Pedroza/header.php
+++ wp-content/themes/Cv-Victor-Pedroza/header.php
@@ -46,35 +46,18 @@
 	      <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
 
          <ul id="nav" class="nav">
-            <li class="current"><a class="smoothscroll" href="<?php echo get_site_url(); ?>/home">Home</a></li>
-            <li><a class="smoothscroll" href="<?php echo get_site_url(); ?>/about">About</a></li>
-	         <li><a class="smoothscroll" href="<?php echo get_site_url(); ?>/resume">Resume</a></li>
-            <li><a class="smoothscroll" href="<?php echo get_site_url(); ?>/work">Works</a></li>
-            <li><a class="smoothscroll" href="<?php echo get_site_url(); ?>/testimonials">Testimonials</a></li>
-            <li><a class="smoothscroll" href="<?php echo get_site_url(); ?>/contact">Contact</a></li>
+            <li class="current"><a href="<?php echo get_site_url(); ?>/home">Home</a></li>
+            <li><a href="<?php echo get_site_url(); ?>/about">About</a></li>
+	         <li><a href="<?php echo get_site_url(); ?>/resume">Resume</a></li>
+            <li><a href="<?php echo get_site_url(); ?>/work">Works</a></li>
+            <li><a href="<?php echo get_site_url(); ?>/testimonials">Testimonials</a></li>
+            <li><a href="<?php echo get_site_url(); ?>/contact">Contact</a></li>
          </ul> <!-- end #nav -->
 
       </nav> <!-- end #nav-wrap -->
 
-      <div class="row banner">
-         <div class="banner-text">
-            <h1 class="responsive-headline">¡Hola! soy Victor Pedroza.</h1>
-            <h3>Soy profesional en <span>Ingenieria de petroleo</span>, <span>desarrolador web</span> y <span>un emprendedor</span> curioso por nuevas tecnologias, estudiando y aprendiendo por mi cuenta para realizar mis metas. <a class="smoothscroll" href="#about">Empieza a desplazarte </a>y lee un poco mas <a class="smoothscroll" href="#about">acerca de mi</a>.</h3>
-            <hr />
-            <ul class="social">
-               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-               <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-               <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-               <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-               <li><a href="#"><i class="fa fa-skype"></i></a></li>
-            </ul>
-         </div>
-      </div>
+      
 
-      <p class="scrolldown">
-         <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
-      </p>
 
-   </header> <!-- Header End -->
+
+  
