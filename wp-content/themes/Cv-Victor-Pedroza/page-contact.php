777fcf2dc820997e1c5a6fbdb5b6bb03ab7c9194
Victor Pedroza <vjpb881104@gmail.com>
2020-05-18 21:23:27 -0500

    fix: Instaled contact form 7 to creat contact page and deleted WPFORMS

--- wp-content/themes/Cv-Victor-Pedroza/page-contact.php
+++ wp-content/themes/Cv-Victor-Pedroza/page-contact.php
@@ -26,46 +26,9 @@
 
       <div class="eight columns">
 
-         <!-- form -->
-         <form action="" method="post" id="contactForm" name="contactForm">
-            <fieldset>
+      <?php echo do_shortcode( '[contact-form-7 id="353" title="Contact form 1"]' );?>
+      
 
-               <div>
-                  <label for="contactName">Name <span class="required">*</span></label>
-                  <input type="text" value="" size="35" id="contactName" name="contactName">
-               </div>
-
-               <div>
-                  <label for="contactEmail">Email <span class="required">*</span></label>
-                  <input type="text" value="" size="35" id="contactEmail" name="contactEmail">
-               </div>
-
-               <div>
-                  <label for="contactSubject">Subject</label>
-                  <input type="text" value="" size="35" id="contactSubject" name="contactSubject">
-               </div>
-
-               <div>
-                  <label for="contactMessage">Message <span class="required">*</span></label>
-                  <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-               </div>
-
-               <div>
-                  <button class="submit">Submit</button>
-                  <span id="image-loader">
-                     <img alt="" src="<?php echo get_bloginfo('template_directory'); ?>/images/loader.gif">
-                  </span>
-               </div>
-
-            </fieldset>
-         </form> <!-- Form End -->
-
-            <!-- contact-warning -->
-      <div id="message-warning"> Error boy</div>
-         <!-- contact-success -->
-            <div id="message-success">
-               <i class="fa fa-check"></i>Your message was sent, thank you!<br>  
-            </div>
    </div>
 
 
