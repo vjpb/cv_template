f0e666d31706c85167d2f81c3ff8174e0ba63cbf
Victor Pedroza <vjpb881104@gmail.com>
2020-05-25 01:03:16 -0500

    fix: Fixed section home.php and deleted some code unnecessary

--- wp-content/themes/Cv-Victor-Pedroza/page-home.php
+++ wp-content/themes/Cv-Victor-Pedroza/page-home.php
@@ -13,28 +13,7 @@
                     </h1>
                     <h3>
                             <?php the_field ( 'text_1' ) ?>
-                        
-                        <span>
-                            <?php the_field ( 'highlighted_text_1' ) ?>
-                        </span>
-
-                            <?php the_field ( 'text_2' ) ?>
-
-                        <span>
-                            <?php the_field ( 'highlighted_text_2' ) ?>
-                        </span>
-
-                            <?php the_field ( 'text_3' ) ?>
-
-                        <span>
-                            <?php the_field ( 'highlighted_text_3' ) ?>
-                        </span>
-
-                            <?php the_field ( 'text_4' ) ?>
-
-                        <span>
-                            <?php the_field ( 'highlighted_text_4' ) ?>
-                        </span>
+                      
                     </h3><!-- Introduction End -->
                     <hr />
                 </div>
